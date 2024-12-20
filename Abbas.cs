6bc080df05140b72217347d6d3dc3c1e4849dc85
Abbas Raza <142605760+Aboss3b13@users.noreply.github.com>
2024-12-20 10:15:20 +0100

    Add files via upload

---
+++ Abbas.cs
@@ -0,0 +1,40 @@
+﻿using Zusammenarbeits_dilemma;
+
+public class Abbas : Player
+{
+    private string _name = "Abbas";
+    private int _points;
+    private int _roundCount;
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public bool FirstMove()
+    {
+        return false;
+    }
+
+    public bool NextMove(bool previousMove)
+    {
+        _roundCount++;
+        Random random = new Random();
+        random.Next(1,3);
+
+        if (random.Next(1, 10) < 7)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ReceivedPoint(int ReceivedPointFromRound)
+    {
+        _points += ReceivedPointFromRound;
+
+    }
+}
